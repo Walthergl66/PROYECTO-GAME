39ec6be035b92308350d8c76248f8c37929b0bf6
Walthergl66 <waltherjr68@gmail.com>
2025-05-27 16:27:23 -0500

    1

--- src/routes/UsuariosR.ts
+++ src/routes/UsuariosR.ts
@@ -1,5 +1,14 @@
-import { Express } from "express";
+import { Router } from "express";
 import { AppDataSource } from "../data-source";
 import { Usuario } from "../models/Usuarios";
 
+const route = Router();
 
+route.get("/", async ( req, res ) => {
+    try {
+        const usuario = await AppDataSource.getRepository(Usuario).find()
+        res.json(usuario)
+    } catch (error){
+        res.status
+    }
+});
