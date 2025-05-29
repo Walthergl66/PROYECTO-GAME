ef5f3797f32e4102e6d03a3ba25aaf133d14fbfe
Walthergl66 <waltherjr68@gmail.com>
2025-05-29 09:28:43 -0500

    cambios para despliegue

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import cors from "cors";
 import path from "path"
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -24,6 +24,7 @@ app.get("/", (req, res) => {
 })
 
 
+const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
 
 // Conectar a la base de datos
 
@@ -31,8 +32,8 @@ AppDataSource.initialize()
   .then(() => {
     console.log("Conectado a la base de datos");
 
-    app.listen(3000, () => {
-      console.log("Servidor corriendo en http://localhost:3000");
-    });
+  app.listen(PORT, () => {
+    console.log(`Servidor backend corriendo en ${BASE_URL}`);
+  });
   })
   .catch((err) => console.error("Error al conectar a la base de datos", err));
