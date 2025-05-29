f5d0b902371899cd3ee0d90fcb493076d368734a
Walthergl66 <waltherjr68@gmail.com>
2025-05-29 08:58:54 -0500

    Migracion de pg a supabase

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,19 +1,16 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { Preguntas } from "./models/Preguntas";
-import dotenv from "dotenv";
-
-dotenv.config();
+require('dotenv').config();
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: process.env.DB_HOST || "localhost",
-    port: Number(process.env.DB_PORT) || 5432,
-    username: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASSWORD || "Hola",
-    database: process.env.DB_NAME || "Game",
-    synchronize: true,
-    logging: false,
-    ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
-    entities: [Preguntas],
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || "5432"),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [Preguntas],
+  synchronize: false, // o true solo en desarrollo
+  logging: false,
 });
