32df6e727ad38da35af3f3cebecce5e61b2515bb
Walthergl66 <waltherjr68@gmail.com>
2025-06-04 18:30:19 -0500

    cambio de puerto

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import cors from "cors";
 import path from "path"
 
 const app = express();
-const PORT = 3000;
+
 
 app.use(cors());
 app.use(express.json());
@@ -27,12 +27,14 @@ app.get("/", (req, res) => {
 
 // Conectar a la base de datos
 
+const PORT = parseInt(process.env.PORT || "3000", 10); // <- aquí
+
 AppDataSource.initialize()
   .then(() => {
     console.log("Conectado a la base de datos");
 
-    app.listen(3000, () => {
-      console.log("Servidor corriendo en http://localhost:3000");
+    app.listen(PORT, () => { // <- y aquí
+      console.log(`Servidor corriendo en http://localhost:${PORT}`);
     });
   })
   .catch((err) => console.error("Error al conectar a la base de datos", err));
