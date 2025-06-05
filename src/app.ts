bb5d1f01ca7e9abd1912888fbf753b06ec686f5e
Walthergl66 <waltherjr68@gmail.com>
2025-06-04 21:22:19 -0500

    puerto online

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import path from "path"
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = Number(process.env.PORT) || 8080;
 
 app.use(cors());
 app.use(express.json());
@@ -31,9 +31,10 @@ AppDataSource.initialize()
     console.log("✅ Conectado a la base de datos");
 
     // 🔈 Escucha en todas las interfaces (necesario para Cloud Run)
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`Servidor corriendo en el puerto ${PORT}`);
     });
+
   })
   .catch((err) => {
     console.error("❌ Error al conectar a la base de datos:", err);
